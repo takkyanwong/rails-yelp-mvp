9a700645c245fbc1998285adb15bd9c233ef9ef2
Tay Yang Wong <wongtakkyan@gmail.com>
2020-10-20 22:04:10 +0200

    savegame 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'seeding stuff'
+5.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: %w[chinese italian japanese french belgian].sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+end
+
+puts 'finished seeding'
